c30962413340915215e3234bc4ace0895613ba6d
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2024-08-14 21:27:45 +0530

    don

--- models/Products.js
+++ models/Products.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -8,4 +8,5 @@ const productSchema = new mongoose.Schema({
   image: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
+ 
